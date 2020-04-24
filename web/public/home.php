46c04e44c025cea8550dab565c69262562cd0159
Jean-Luc Aubert <jean-luc.a@web-projet.com>
2020-04-24 14:57:28 +0200

    Added form processing

--- web/public/home.php
+++ web/public/home.php
@@ -4,6 +4,6 @@ session_start();
 if (!array_key_exists("id", $_SESSION)) {
     header("Location: index.php");
 } else {
-  echo "Bienvenue utilisateur";  
+  include("./views/signoff.php");
 }
 
--- web/public/signin.php
+++ web/public/signin.php
@@ -4,10 +4,56 @@
 * @author Adrar - Apr. 2020
 * @version 1.0.0
 *   Traite le formulaire de login
+* @todo Faire en sorte de définir les champs du formulaire à partir de PHP !
 */
 session_start();
 
-if ($_POST["login"] == "" || $_POST["password"] == "") {
+/**
+ * 
+ * @var array $fields
+ * 
+ * Sets the fields variables
+ */
+$fields = [
+    "login" => [
+        "required" => true,
+    ],
+    "password" => [
+        "required" => true
+    ]
+];
+
+/**
+ * 
+ * @var boolean $isFormValid
+ * 
+ * Détermine la validité du formulaire
+ */
+$isFormValid = true;
+
+// Loop over fields
+/**
+ * Occurrence 1 :
+ *  $key = "login"
+ *  $value = ["required" => true]
+ *  => $_POST[$key] <=> $_POST["login"]
+ * Occurrence 2 :
+ *  $key = "password"
+ *  $value = ["required" => true]
+ *  => $_POST[$key] <=> $_POST["password"]
+ */
+foreach ($fields as $key => $value) {
+    if (array_key_exists("required", $value) && $value["required"] === true) {
+        // C'est bien un champ requis !
+        if ($_POST[$key] == "") {
+            $isFormValid = false;
+            break;
+        }
+    }
+}
+
+
+if ($isFormValid === false) {
     // L'utilisateur est un âne !
     header("Location: index.php?error=yes");
 } else {
