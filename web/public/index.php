5feb24a2599fde52bd7e5c9cea7e900b75b69a55
Jean-Luc Aubert <jean-luc.a@ideafactory.fr>
2020-05-05 07:35:17 +0200

    Variables, conditions and loops

--- web/public/index.php
+++ web/public/index.php
@@ -1,65 +1,136 @@
 <?php
-/**
+/*
  * @name index.php
- * @author Adrar - Apr. 2020
+ * @author Adrar - May. 2020
  * @version 1.0.0
- * 	Illustrate some basics of PHP
+ * 	App dispatcher : load controller according to context
  */
 
-require_once("./Core/DBAL/MySQL.php");
-require_once("./models/User.php");
-require_once("./Repositories/UserRepository.php");
+/**
+ * Tâches de ma todolist
+ * @var array $taches
+ */
+$taches = [
+    "Apprendre PHP",
+    "Comprendre les concepts Objets",
+    "Créer une page HTML en PHP",
+    "S'approprier les boucles",
+    "Lire le journal",
+    "Préparer l'apéro de déconfinement",
+    "Apprendre le tricot"
+];
 
-session_start();
+/**
+ * Durées définies pour les tâches
+ * @var array $durees
+ */
+$durees = [
+  15,
+  15,
+  5,
+  1,
+  0.5,
+  2,
+  365
+];
 
-if (array_key_exists("id", $_SESSION)) {
-    header("Location: home.php");
+$tasks = [
+    ["Apprendre PHP", 15],
+    ["Comprendre les concepts Objets", 15],
+    ["Créer une page HTML en PHP", 5],
+    ["S'approprier les boucles", 1],
+    ["Lire le journal", 0.5],
+    ["Préparer l'apéro de déconfinement", 2],
+    ["Apprendre le tricot", 365]
+];
+
+// Problème 1 : Afficher une liste HTML des tâches à réaliser (foreach...)
+$htmlTaskList = "<ul>"; // "<ol>"
+foreach ($taches as $tache) {
+    $htmlTaskList .= "<li>" . $tache . "</li>";
+}
+$htmlTaskList .= "</ul>";
+
+
+// Problème 2 : Afficher dans un paragraphe HTML (<p></p>) 
+// les tâches et la durée de la tâche dont la durée est supérieure ou égale à 15 (for... if...) ou (foreach... if...)
+$paragraph = "";
+for ($indice = 0; $indice < count($taches); $indice++) {
+    if ($durees[$indice] >= 15) {
+        $paragraph .= "<p>Tâche : " . $taches[$indice] . " [" . $durees[$indice] . "]</p>";
+    }
 }
 
-$error = "";
 
-if (array_key_exists("error", $_GET)) {
-    $error = "Veuillez remplir le formulaire correctement !";
+// Problème 3 : Créer un document JSON reprenant chaque tâche et la durée associée (for... if...) ou (foreach... if...)
+//  [
+//      {
+//          "tache": "Apprendre PHP",
+//          "duree": 15
+//      },
+//      {
+//          ...
+//      }
+//  ]
+$indice = 0; // Initialisation d'un compteur
+$jsonFile = "[\n";
+foreach ($taches as $tache) {
+    $jsonFile .= "\t{\n"; // Début de la structure
+    $jsonFile .= "\"tache\":\"" . $tache . "\",\n"; // "tache": "Apprendre le PHP"
+    $jsonFile .= "\"duree\":\"" . $durees[$indice] . "\"\n";
+    $jsonFile .= "},\n";
+    
+    $indice++;
+}
+$jsonFile = substr($jsonFile, 0, strlen($jsonFile) - 2); // Supprime les deux derniers caractères
+$jsonFile .= "\n]";
+
+
+// Problème 4 : utiliser $tasks pour afficher la liste des tâches, leurs durées et mettre en gras les tâches
+//  dont la durée est supérieure ou égale à 15 jours
+$resultat = "";
+for ($indice = 0; $indice < count($tasks); $indice++) {
+    if ($tasks[$indice][1] >= 15) {
+        $resultat .= "<strong>" . $tasks[$indice][0] . "</strong> [" . $tasks[$indice][1] . "]";
+    } else {
+        $resultat .= $tasks[$indice][0] . "[" . $tasks[$indice][1] . "]";
+    }
 }
 
+/**
+ * $indice <- 0
+ *  $tasks[$indice] <=> $tasks[0] => ["Apprendre PHP", 15]
+ *  $tasks[$indice][1] <=> $tasks[0][1] => 15
+ */
 
+// Générer un vrai document HTML
+echo "<!doctype html>";
+echo "<html>";
+echo "<head>";
+echo "<meta charset=\"utf-8\">";
+echo "<title>Test Encoding</title>";
+echo "</head>";
+echo "<body>";
+echo $htmlTaskList; // Ce n'est pas une sortie écran proprement dite !
+echo $paragraph;
+echo "<code><pre>" . $jsonFile . "</pre></code>";
+echo $resultat;
+echo "</body>";
+echo "</html>";
 
-$user = new User();
-$user->lastname = "Talut";
-$user->firstname = "Jean";
-$user->email = "roues@jantes.com";
-$user->username = "tuning";
-$user->password = "gninut";
-
-$repository = new UserRepository($user);
-$repository->create();
-
-?>
-
-<!doctype html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title><?php echo $greetings;?></title>
-	</head>
-	
-	<body>
-		<form method="post" action="signin.php">
-			<label>Utilisateur :</label>
-			<input type="text" name="login">
-			<label>Mot de passe :</label>
-			<input type="password" name="password">
-			<button>
-				Connexion
-			</button>
-			<a href="forgot_password.php">Mot de passe oublié ?</a>
-			<a href="./controllers/register.php">S'inscrire</a>
-		</form>
-		<?php 
-		if (strlen($error)) {
-		    echo $error;
-		}
-		?>
-	</body>
-</html>
+/**
+ * $indice <- 0
+ *  $jindice <- 0
+ *      $tasks[$indice] <=> $tasks[0] => ["J'apprends PHP", 15]
+ *      $tasks[$jindice] <=> $tasks[0] => ["J'apprends PHP", 15]
+ *  $jindice <- 1
+ *      $tasks[$indice] <=> $tasks[0] => ["J'apprends PHP", 15]
+ *      $tasks[$jindice] <=> $tasks[1] => ["Comprendre les concepts Objets", 15]
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
 
