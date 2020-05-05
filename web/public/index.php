17ee86d87af14b4d0f081005b3bcb513cc20bf08
Jean-Luc Aubert <jean-luc.a@ideafactory.fr>
2020-05-05 07:55:15 +0200

    Some other output type, and headers settings

--- web/public/index.php
+++ web/public/index.php
@@ -52,15 +52,18 @@ foreach ($taches as $tache) {
 $htmlTaskList .= "</ul>";
 
 
-// Problème 2 : Afficher dans un paragraphe HTML (<p></p>) 
+// Problème 2 : Créer un document XML 
 // les tâches et la durée de la tâche dont la durée est supérieure ou égale à 15 (for... if...) ou (foreach... if...)
-$paragraph = "";
+$xml = "<tasks>\n";
 for ($indice = 0; $indice < count($taches); $indice++) {
     if ($durees[$indice] >= 15) {
-        $paragraph .= "<p>Tâche : " . $taches[$indice] . " [" . $durees[$indice] . "]</p>";
+        $xml .= "\t<task>\n";
+        $xml .= "\t\t<libelle>" . $taches[$indice] . "</libelle>\n";
+        $xml .= "\t\t<duration>" . $durees[$indice] . "</duration>\n";
+        $xml .= "</task>\n";
     }
 }
-
+$xml .= "</tasks>";
 
 // Problème 3 : Créer un document JSON reprenant chaque tâche et la durée associée (for... if...) ou (foreach... if...)
 //  [
@@ -76,9 +79,9 @@ $indice = 0; // Initialisation d'un compteur
 $jsonFile = "[\n";
 foreach ($taches as $tache) {
     $jsonFile .= "\t{\n"; // Début de la structure
-    $jsonFile .= "\"tache\":\"" . $tache . "\",\n"; // "tache": "Apprendre le PHP"
-    $jsonFile .= "\"duree\":\"" . $durees[$indice] . "\"\n";
-    $jsonFile .= "},\n";
+    $jsonFile .= "\t\t\"tache\":\"" . $tache . "\",\n"; // "tache": "Apprendre le PHP"
+    $jsonFile .= "\t\t\"duree\":\"" . $durees[$indice] . "\"\n";
+    $jsonFile .= "\t},\n";
     
     $indice++;
 }
@@ -88,14 +91,18 @@ $jsonFile .= "\n]";
 
 // Problème 4 : utiliser $tasks pour afficher la liste des tâches, leurs durées et mettre en gras les tâches
 //  dont la durée est supérieure ou égale à 15 jours
-$resultat = "";
+$resultat = "<ol>";
 for ($indice = 0; $indice < count($tasks); $indice++) {
+    $resultat .= "<li>";
     if ($tasks[$indice][1] >= 15) {
-        $resultat .= "<strong>" . $tasks[$indice][0] . "</strong> [" . $tasks[$indice][1] . "]";
+        $resultat .= "<strong>" . $tasks[$indice][0] . "</strong>";
     } else {
-        $resultat .= $tasks[$indice][0] . "[" . $tasks[$indice][1] . "]";
+        $resultat .= $tasks[$indice][0];
     }
+    $resultat .= " [" . $tasks[$indice][1] . "]";
+    $resultat .= "</li>";
 }
+$resultat .= "</ol>";
 
 /**
  * $indice <- 0
@@ -104,19 +111,24 @@ for ($indice = 0; $indice < count($tasks); $indice++) {
  */
 
 // Générer un vrai document HTML
-echo "<!doctype html>";
-echo "<html>";
-echo "<head>";
-echo "<meta charset=\"utf-8\">";
-echo "<title>Test Encoding</title>";
-echo "</head>";
-echo "<body>";
-echo $htmlTaskList; // Ce n'est pas une sortie écran proprement dite !
-echo $paragraph;
-echo "<code><pre>" . $jsonFile . "</pre></code>";
-echo $resultat;
-echo "</body>";
-echo "</html>";
+$html = "";
+$html .= "<!doctype html>";
+$html .=  "<html>";
+$html .=  "<head>";
+$html .=  "<meta charset=\"utf-8\">";
+$html .=  "<title>Test Encoding</title>";
+$html .=  "</head>";
+$html .=  "<body>";
+$html .=  $htmlTaskList; // Ce n'est pas une sortie écran proprement dite !
+$html .=  "<code><pre>" . $xml . "</pre></code>";
+$html .=  "<code><pre>" . $jsonFile . "</pre></code>";
+$html .=  $resultat;
+$html .=  "</body>";
+$html .=  "</html>";
+
+// Send headers and content to Apache (i.e Output content to the client)
+header("Content-Type: text/html", false, 200);
+echo $html;
 
 /**
  * $indice <- 0
