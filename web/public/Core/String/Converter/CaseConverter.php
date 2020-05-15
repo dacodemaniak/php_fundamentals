867d963ab8a2526bcf66bcaa3b34aa3b01eec593
Jean-Luc Aubert <jean-luc.a@ideafactory.fr>
2020-05-15 08:50:05 +0200

    Add toSnakeCase method in CaseConverter class

--- web/public/Core/String/Converter/CaseConverter.php
+++ web/public/Core/String/Converter/CaseConverter.php
@@ -11,5 +11,14 @@ class CaseConverter extends \PHPString
     public function toCamelCase(string $separator = "_"): string {
         return str_replace(" ", "", lcfirst(ucwords(str_replace($separator, " ", $this->string))));
     }
+    
+    public function toSnakeCase(): string {
+        preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $this->string, $matches);
+        $snake = $matches[0];
+        foreach ($snake as &$match) {
+            $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
+        }
+        return implode('_', $snake);
+    }
 }
 
